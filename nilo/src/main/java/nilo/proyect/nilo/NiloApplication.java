65697b221c9e6af81515a12a98ad4ce47a1dfa8b
Pialeonor12# <jorgeadrian_pucheta@hotmail.com>
2024-10-14 20:16:51 -0300

    first push[A

---
+++ nilo/src/main/java/nilo/proyect/nilo/NiloApplication.java
@@ -0,0 +1,13 @@
+package nilo.proyect.nilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiloApplication.class, args);
+	}
+
+}
---
+++ nilo/src/test/java/nilo/proyect/nilo/NiloApplicationTests.java
@@ -0,0 +1,13 @@
+package nilo.proyect.nilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
